4231f823a7c5697203078b6ee2ceaa907920ac62
flaskfarm <flaskfarm@gmail.com>
2022-11-14 18:32:47 +0900

    update

--- setup.py
+++ setup.py
@@ -30,6 +30,7 @@ except Exception as e:
     P.logger.error(traceback.format_exc())
 
 from . import Auth
-P.get_auth_status = Auth.get_auth_status()
+
+P.get_auth_status = Auth.get_auth_status
 
 # python -m flaskfarm.cli.code_encode --source=C:\work\FlaskFarm\data\LOADING2\sjva
